222737bc5d10213f97f4580ff5f8d4d3308379ba
Carlos Villera <carlosmva2009@hotmail.com>
2020-04-29 18:18:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,17 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <audio controls><source src="http://radiohd.streaminghd.co:7226/live?1588198776" type="audio/ogg"/></audio>
       </header>
     </div>
   );
